98b943650d0172eb9372bf0463e8a457fa3eefb3
MrHacker002 <maxusoid@gmail.com>
2023-11-18 16:32:16 +0200

    requirements/venv

--- socialmedia/users/views.py
+++ socialmedia/users/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, redirect
-# from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.contrib.auth import login ,logout
 from django.contrib.auth.views import LoginView 
 from users.forms import RegisterCustomUserForm
 from django.contrib.auth.forms import AuthenticationForm
 
-# Create your views here.
+
 class RegisterUserView(CreateView):
     form_class = RegisterCustomUserForm
     template_name = 'users/register.html'
@@ -15,13 +14,12 @@ class RegisterUserView(CreateView):
         login(self.request, user=user)
         return redirect('home')
 
+
 class LoginUserView(LoginView):
     form_class = AuthenticationForm
     template_name = 'users/login.html'
 
 
-
 def exit(request):
     logout(request)
     return redirect('home')
-
