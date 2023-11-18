fcff9bf6f0da6e7cfa994be877e7ab300f9182cc
MrHacker002 <maxusoid@gmail.com>
2023-11-18 17:30:24 +0200

    static

--- socialmedia/users/models.py
+++ socialmedia/users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.core.mail import send_mail
 from django.contrib.auth.models import PermissionsMixin, UserManager
 from django.contrib.auth.base_user import AbstractBaseUser
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from users.managers import UserManager
 
 
